bdecd76c518d86ab7f5ffd9354934ccf8a879716
prastoin <paul@twenty.com>
2026-02-13 17:29:22 +0100

    fix

--- packages/twenty-front/src/generated-metadata/graphql.ts
+++ packages/twenty-front/src/generated-metadata/graphql.ts
@@ -2268,7 +2268,7 @@ export type Mutation = {
   startChannelSync: ChannelSyncSuccess;
   switchBillingPlan: BillingUpdateOutput;
   switchSubscriptionInterval: BillingUpdateOutput;
-  syncApplication: Scalars['Boolean'];
+  syncApplication: WorkspaceMigrationDto;
   trackAnalytics: Analytics;
   uninstallApplication: Scalars['Boolean'];
   updateApiKey?: Maybe<ApiKey>;
@@ -5172,6 +5172,12 @@ export enum WorkspaceMigrationActionType {
   delete = 'delete',
   update = 'update'
 }
+
+export type WorkspaceMigrationDto = {
+  __typename?: 'WorkspaceMigrationDTO';
+  actions: Scalars['JSON'];
+  applicationUniversalIdentifier: Scalars['String'];
+};
 
 export type WorkspaceMigrationDeleteActionInput = {
   metadataName: AllMetadataName;
